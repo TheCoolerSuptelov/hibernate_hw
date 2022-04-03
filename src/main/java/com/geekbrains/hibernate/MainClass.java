6aae1fd6ef1fae52f99b0fa99d9e00c5f7bc26cf
Кирилл Суптелов <suptelov@gmail.com>
2022-04-03 19:47:13 +0700

    modified:   src/main/java/com/geekbrains/hibernate/Items.java 	modified:   src/main/java/com/geekbrains/hibernate/MainClass.java 	modified:   src/main/resources/hibernate.cfg.xml 	modified:   target/classes/com/geekbrains/hibernate/MainClass.class 	modified:   target/classes/hibernate.cfg.xml

--- src/main/java/com/geekbrains/hibernate/MainClass.java
+++ src/main/java/com/geekbrains/hibernate/MainClass.java
@@ -1,15 +1,81 @@
 package com.geekbrains.hibernate;
 
+import org.hibernate.LockMode;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class MainClass {
     public static void main(String[] args) {
+        //hw11();
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        configuration.addAnnotatedClass(Items.class);
+        SessionFactory factory = configuration.buildSessionFactory();
+        Session session = factory.getCurrentSession();
+        session.beginTransaction();
+        for (int i = 0; i < 40; i++) {
+            Items newItem = new Items();
+            session.save(newItem);
+        }
+        session.getTransaction().commit();
 
+        //debugThreadMove(factory);
+        threadMove(factory);
+    }
+
+    private static void debugThreadMove(SessionFactory factory) {
+        AtomicInteger globalCounter = new AtomicInteger(160_000);
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+        Session session = factory.getCurrentSession();
+        session.beginTransaction();
+        Items item = (Items) session.get(Items.class, getRandomNumberUsingInts(), LockMode.PESSIMISTIC_WRITE);
+        item.increaseCounter();
+        session.save(item);
+        session.getTransaction().commit();
+        globalCounter.decrementAndGet();
+    }
+
+    private static void threadMove(SessionFactory factory) {
+        AtomicInteger globalCounter = new AtomicInteger(160_000);
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+
+        for (int i = 0; i < 8; i++) {
+            new Thread(() -> {
+                while (globalCounter.get() > 0) {
+                    try {
+                        Session session = factory.getCurrentSession();
+                        session.beginTransaction();
+                        Items item = (Items) session.get(Items.class, getRandomNumberUsingInts(), LockMode.PESSIMISTIC_WRITE);
+                        item.increaseCounter();
+                        session.save(item);
+                        session.getTransaction().commit();
+                        globalCounter.decrementAndGet();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+            );
+        }
+    }
+
+    public static long getRandomNumberUsingInts() {
+        Random random = new Random();
+        return (long) random.ints(0, 39)
+                .findFirst()
+                .getAsInt();
+    }
+
+    private static void hw11() {
         Configuration configuration = new Configuration();
         configuration.configure("hibernate.cfg.xml");
         configuration.addAnnotatedClass(Customers.class);
@@ -42,7 +108,7 @@ public class MainClass {
         Session curSession = factory.getCurrentSession();
         curSession.beginTransaction();
         // System.out.println(chickenFillet.whoBoughtProduct(curSession));
-        System.out.println(chickenFillet.getCustomersList().stream().map(customer->customer.getName()).collect(Collectors.joining(", ")));
+        System.out.println(chickenFillet.getCustomersList().stream().map(customer -> customer.getName()).collect(Collectors.joining(", ")));
         System.out.println(customerHo.showPurchaseHistory());
         curSession.getTransaction().commit();
     }
